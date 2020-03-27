9f19d0d2f038ac7438a76311906659f375b33b98
Christopher Viel <viel.christopher@gmail.com>
2020-03-27 00:05:43 -0400

    Refactor and better error handling

--- custom_components/lovelace_gen/__init__.py
+++ custom_components/lovelace_gen/__init__.py
@@ -22,24 +22,29 @@ class TemplateConstructor(object):
         self.config.update(config)
 
     def __call__(self, loader, node):
-        variables = {}
-        if isinstance(node.value, str):
+        filename, variables = self._read_tag(loader, node)
+        stream = self._render_template(filename, variables)
+
+        try:
+            document = yaml.load(stream, SafeLineLoader) or OrderedDict()
+            return _add_reference(document, loader, node)
+        except yaml.YAMLError as ex:
+            _LOGGER.error("Unable to parse rendered YAML in %s: %s", filename, ex)
+            raise HomeAssistantError(exc)
+
+    def _read_tag(self, loader, node):
+        if isinstance(node, yaml.ScalarNode):
             filename = node.value
+            variables = {}
         else:
             mapping = loader.construct_mapping(node, deep=True)
             filename = mapping.get("file")
             variables = mapping.get("variables", {})
 
         filename = os.path.abspath(os.path.join(os.path.dirname(loader.name), filename))
+        return filename, variables
 
-        try:
-            rendered = self._render_templated_yaml(filename, variables)
-            return _add_reference(rendered, loader, node)
-        except FileNotFoundError as exc:
-            _LOGGER.error("Unable to include file %s: %s", filename, exc)
-            raise HomeAssistantError(exc)
-
-    def _render_templated_yaml(self, filename, variables={}):
+    def _render_template(self, filename, variables):
         try:
             template = None
             with open(filename, encoding="utf-8") as file:
@@ -50,20 +55,17 @@ class TemplateConstructor(object):
             rendered = template.render({**variables, "_global": self.config.get("variables", {})})
             stream = io.StringIO(rendered)
             stream.name = filename
-            return yaml.load(stream, Loader=SafeLineLoader) or OrderedDict()
-        except TemplateError as exc:
-            _LOGGER.error(str(exc))
-            raise HomeAssistantError(exc)
-        except yaml.YAMLError as exc:
-            _LOGGER.error(str(exc))
-            raise HomeAssistantError(exc)
-        except UnicodeDecodeError as exc:
-            _LOGGER.error("Unable to read file %s: %s", filename, exc)
-            raise HomeAssistantError(exc)
+            return stream
+        except (FileNotFoundError, UnicodeDecodeError) as ex:
+            _LOGGER.error("Unable to read file %s: %s", filename, ex)
+            raise HomeAssistantError(ex)
+        except TemplateError as ex:
+            _LOGGER.error("Unable to render file %s: %s", filename, ex)
+            raise HomeAssistantError(ex)
 
 
 constructor = TemplateConstructor()
 def setup(hass, config):
     constructor.setup(hass, config.get(DOMAIN))
-    SafeLineLoader.add_constructor(constructor.yaml_tag, constructor)
+    yaml.SafeLoader.add_constructor(constructor.yaml_tag, constructor)
     return True
